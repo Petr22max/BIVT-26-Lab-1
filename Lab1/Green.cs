91e027a8adcdae95dde9cb7a6489b438df227858
Чайковский Петр <Petor220110@gmail.com>
2026-09-10 12:21:42 +0300

    Update Green.cs

--- Lab1/Green.cs
+++ Lab1/Green.cs
@@ -7,6 +7,7 @@
             bool answer = false;
 
             // code here
+            answer = Math.Abs(d) >= 1;
 
             // end
 
@@ -17,6 +18,7 @@
             bool answer = false;
 
             // code here
+            answer = (d + f) / 2 > 0;
 
             // end
 
@@ -27,6 +29,7 @@
             bool answer = false;
 
             // code here
+            answer = a + b > (Math.Abs(a) + Math.Abs(b)) / 2;
 
             // end
 
@@ -37,6 +40,13 @@
             int answer = 0;
 
             // code here
+            answer = a;
+
+            if (b > answer)
+                answer = b;
+
+            if (c > answer)
+                answer = c;
 
             // end
 
@@ -47,6 +57,10 @@
             double answer = 0;
 
             // code here
+            if (Math.Abs(x) <= 1)
+                answer = x * x - 1;
+            else
+                answer = 0;
 
             // end
 
@@ -57,6 +71,8 @@
             bool answer = false;
 
             // code here
+            if (y >= 0 && y <= 1 - Math.Abs(x))
+                answer = true;
 
             // end
 
@@ -68,6 +84,8 @@
             bool answer = true;
 
             // code here
+            if (n < 0 || n % 2 == 0)
+                answer = false;
 
             // end
 
@@ -78,6 +96,10 @@
             bool answer = false;
 
             // code here
+            int c = (X + 1) / 2;
+
+            if (X >= 7 && c * Y >= 240 && c * Y <= 360)
+                answer = true;
 
             // end
 
